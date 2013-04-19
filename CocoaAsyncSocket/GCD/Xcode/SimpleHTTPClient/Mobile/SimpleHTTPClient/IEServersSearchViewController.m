fbae2c9a08ca0d41aea63d28dcecdb7ffc121a4b
ddk8242 <ddk8242@gmail.com>
2013-04-18 21:15:02 -0400

    Table editable when longPressed

--- CocoaAsyncSocket/GCD/Xcode/SimpleHTTPClient/Mobile/SimpleHTTPClient/IEServersSearchViewController.m
+++ CocoaAsyncSocket/GCD/Xcode/SimpleHTTPClient/Mobile/SimpleHTTPClient/IEServersSearchViewController.m
@@ -13,6 +13,7 @@
 
 @property (weak, nonatomic) IBOutlet UITableView *searchResults;
 @property (strong, nonatomic) IBOutlet NSMutableArray *searchItems;
+@property (nonatomic, weak) UILongPressGestureRecognizer *longReco;
 
 @end
 
@@ -25,7 +26,6 @@
         // Custom initialization
     }
     return self;
-
 }
 
 - (void)viewDidLoad
@@ -33,7 +33,16 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
     NSLog(@"In Servers Search");
+    
+    UILongPressGestureRecognizer *reco = [[UILongPressGestureRecognizer alloc]initWithTarget:self
+                                                                                      action:@selector(handleLongPress:)];
+    [self.searchResults addGestureRecognizer:reco];
+    _longReco = reco;
+    
     _searchItems = [NSMutableArray array];
+    _searchResults.delegate = self;
+    _searchResults.dataSource = self;
+    _searchResults.bounces = NO;
 }
 
 - (void)didReceiveMemoryWarning
@@ -43,6 +52,19 @@
 }
 
 #pragma mark tableView methods
+
+-(void)handleLongPress:(UILongPressGestureRecognizer *)reco{
+
+    if (reco.state ==UIGestureRecognizerStateBegan) {
+
+        [_searchResults setEditing:YES animated:YES];
+    }
+    else if (reco.state == (UIGestureRecognizerStateEnded | UIGestureRecognizerStateCancelled | UIGestureRecognizerStateFailed)){
+    
+        [_searchResults setEditing:NO animated:YES];
+    }
+}
+
 -(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
 
     return 1;
@@ -56,6 +78,7 @@
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
 
 //currently iniitalizing the view controller to bring up the gameplay controller
+    
     IEGamePlayScreenViewController *gamePlayCtrl = [[UIStoryboard storyboardWithName:@"MainStory-iPad" bundle:[NSBundle mainBundle]]instantiateViewControllerWithIdentifier:@"iEGamePlay"];
     
     [self presentViewController:gamePlayCtrl
@@ -76,12 +99,52 @@
     return cell;
 }
 
+-(BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath{
+
+//    Use this to stop moving of certain rows
+    NSLog(@"returning yes...");
+    return YES;
+}
+
+-(void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath{
+
+    NSNumber *obj = [[self.searchItems objectAtIndex:[sourceIndexPath row]]copy];
+    [self.searchItems removeObjectAtIndex:[sourceIndexPath row]];
+    [self.searchItems insertObject:obj atIndex:[destinationIndexPath row]];
+    
+    [tableView setEditing:NO animated:YES];
+    
+    NSLog(@"trying...");
+}
+
+//-(UITableViewCellEditingStyle )tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath{
+//
+//    return UITableViewCellEditingStyle;
+//}
+
+-(BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath{
+
+    return YES;
+}
+
+-(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+
+    NSLog(@"editing style for row %d is %d",[indexPath row], editingStyle);
+    
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        [self.searchItems removeObjectAtIndex:[indexPath row]];
+        
+        [_searchResults reloadData];
+    }
+}
+
 #pragma mark -
 
 - (IBAction)connectPressed:(id)sender {
 
     //generating random number
     //fillin the searchItems with that
+    _searchItems = [NSMutableArray array];
     int random = arc4random() %10 +1;
     for (int i=0; i<random; i++) {
         [_searchItems addObject:@(i)];
