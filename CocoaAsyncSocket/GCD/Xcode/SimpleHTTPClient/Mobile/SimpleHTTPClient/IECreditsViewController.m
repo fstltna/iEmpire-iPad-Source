dc5613d361e6c54426e5e5b46686f15e36b766a8
Marisa Giancarla <fstltna@me.com>
2013-09-03 23:48:17 -0700

    Removed excess buttons

--- CocoaAsyncSocket/GCD/Xcode/SimpleHTTPClient/Mobile/SimpleHTTPClient/IECreditsViewController.m
+++ CocoaAsyncSocket/GCD/Xcode/SimpleHTTPClient/Mobile/SimpleHTTPClient/IECreditsViewController.m
@@ -7,18 +7,12 @@
 //
 
 #define SEGUE_IEMPIRE @"iEmpireClient"
-#define SEGUE_IEMPIRE_DIR @"iEmpireDirectory"
-#define SEGUE_OPENSIM_CITY @"OpenSImCity"
-#define SEGUE_OPENSIM_RADIO @"OpenSim Radio"
 #define SEGUE_VEDRAN @"VedranLinkedIn"
 #define SEGUE_MARISA @"MarisaLinkedIn"
 #define SEGUE_DHAVAL @"DhavalLinkedIn"
 
-#define SEGUE_IEMPIRE_LINK @"https://github.com/fstltna/iEmpire-iPad-Source"
-#define SEGUE_IEMPIRE_DIR_LINK @"https://empiredirectory.net"
-#define SEGUE_OPENSIM_CITY_LINK @"https://opensimcity.org"
-#define SEGUE_OPENSIM_RADIO_LINK @"http://radio.opensimcity.org:8188"
-#define SEGUE_VEDRAN_LINK @"http://www.linkedin.com/in/marisagiancarla"
+#define SEGUE_IEMPIRE_LINK @"http://hungrylucy.com"
+#define SEGUE_VEDRAN_LINK @"http://www.freelancer.com/u/the1337code.html"
 #define SEGUE_MARISA_LINK @"http://www.linkedin.com/in/marisagiancarla"
 #define SEGUE_DHAVAL_LINK @"http://www.linkedin.com/in/marisagiancarla"
 
@@ -59,16 +53,6 @@
     NSString *strToUse;
     if ([segue.identifier isEqualToString:SEGUE_IEMPIRE]) {
         strToUse = SEGUE_IEMPIRE_LINK;
-    }
-    else if ([segue.identifier isEqualToString:SEGUE_IEMPIRE_DIR]){
-        strToUse = SEGUE_IEMPIRE_DIR_LINK;
-    }
-    else if ([segue.identifier isEqualToString:SEGUE_OPENSIM_CITY]){
-        
-        strToUse = SEGUE_OPENSIM_CITY_LINK;
-    }
-    else if ([segue.identifier isEqualToString:SEGUE_OPENSIM_RADIO]){
-        strToUse = SEGUE_OPENSIM_RADIO_LINK;
     }
     else if ([segue.identifier isEqualToString:SEGUE_MARISA]){
         strToUse = SEGUE_MARISA_LINK;
